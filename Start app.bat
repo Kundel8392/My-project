3af75f5168bd1adb089da283128dee8f5b573502
Kundel8392 <magda.orzol82@gmail.com>
2024-09-14 13:51:12 +0200

    Create Start app.bat

---
+++ Start app.bat
@@ -0,0 +1,12 @@
+@echo off
+:start
+start charmap.exe
+start calc.exe
+start explorer.exe
+start firefox.exe
+start chrome.exe
+start opera.exe
+start opera gx.exe
+start microsoft edge.exe
+start microsoft teams.exe
+start microsoft store.exe
